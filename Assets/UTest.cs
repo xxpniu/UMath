e7f076971ac0d14a21a74ddf61b43c12ced83c80
xxp-macbook-pro <54249636@qq.com>
2016-12-06 23:53:02 +0800

    add header for test. rotation update

--- Assets/UTest.cs
+++ Assets/UTest.cs
@@ -11,43 +11,47 @@ public class UTest : MonoBehaviour {
        
 	}
 
+    [Header("Input")]
     public Vector3 cForward;
-
     public Vector3 inputVer = Vector3.forward;
     public float angle =0;
     public Vector3 trans;
     public Vector3 scale = Vector3.one;
 
 
-
+    [Header("AngleAxis")]
     public Quaternion q;
     public UQuaternion mq;
 
+    [Header("Euler")]
     public Quaternion qEuler;
     public Vector3 qToEuler;
     public UQuaternion mqEuler;
     public UVector3 mqToEuler;
 
-    public Quaternion eq;
-    public UQuaternion emq;
-
+  
+    [Header("STR Matrix")]
     public Matrix4x4 m;
     public UMatrix4x4 mm;
 
+    [Header("Decompose matrix")]
     public UVector3 exTrans;
     public UVector3 exScale;
     public UQuaternion exRotation;
 
+    [Header("Look At")]
     public Quaternion lookat;
     public UQuaternion mlookat;
     public UVector3 mforwardLookat;
     public Vector3 forwardLookat;
 
+    [Header("Look at to euler")]
     public UVector3 mangle;
     public Vector3 rangle;
     public Matrix4x4 lookatMat;
     public UMatrix4x4 mlookatMat;
 
+    [Header("Delta Angle")]
     public float angleY = 0f;
     public float angleDelta =0f;
     public float angleDeltaM =0f;
@@ -68,10 +72,7 @@ public class UTest : MonoBehaviour {
         qToEuler = qEuler.eulerAngles;
         mqEuler = UQuaternion.Euler(inputUv);
         mqToEuler = mqEuler.eulerAngles;
-
-        eq = Quaternion.Euler(inputVer);
-        emq = UQuaternion.Euler(inputUv);
-
+       
         m = Matrix4x4.TRS(trans, q, scale);
         mm = UMatrix4x4.TRS(t, mq, s);
         exScale = mm.Scale;
@@ -89,6 +90,8 @@ public class UTest : MonoBehaviour {
 
 
         mlookatMat = UMatrix4x4.LookAt(UVector3.zero, inputUv,UVector3.up);
+        lookatMat = Matrix4x4.zero;//need
+
 
         angleY = MathHelper.CalAngleWithAxisY(inputUv);
         angleDeltaM = MathHelper.DeltaAngle(angleY, angle);
